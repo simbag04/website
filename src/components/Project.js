48d9ff15393d45f34d06d691cc83974f165eeda4
simbag04 <simbag04@gmail.com>
2023-07-07 00:37:52 -0700

    Fix project descriptions, add basic image styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,7 +1,21 @@
+
 const Project = (props) => {
     return (
-        <div>
-            <div className="project-title">{props.title}</div>
+        <div className="project">
+            <div className='info'>
+                <div className="project-title">{props.title}</div>
+                <div className="subsection">
+                    <div className="subtitle">Description</div>
+                    <div>{props.description}</div>
+                </div>
+                <div className="subsection">
+                    <div className="subtitle">Technologies</div>
+                    <div>{props.technologies}</div>
+                </div>
+            </div>
+            <div className='image'>
+                <img src={props.img} alt="" sizes="100vw"></img>
+            </div>
         </div>
     )
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,22 @@
 import Project from "./Project"
+import Waldo from '../images/wheres-waldo.png'
+import Library from '../images/library.png'
+import '../styles/Project.css'
 
 const Projects = () => {
     return (
-        <div>
+        <div className="projects">
             <div className="title">Projects</div>
             <ul>
-                <li><Project title="Where's Waldo"></Project></li>
-                <li><Project title="Library"></Project></li>
+                <li><Project title="Where's Waldo" 
+                            img={Waldo} 
+                            description="A photo tagging app inspired by the game 'Where's Waldo'. Features a leaderboard as well."
+                            technologies="React.js, HTML, CSS, Firebase">
+                    </Project></li>
+                <li><Project title="Library" 
+                            img={Library}
+                            description="An app where users can track books - users can add and remove books from their library, change the 'read' status, and also save their information by logging in"
+                            technologies="HTML, CSS, JavaScript, Firebase"></Project></li>
             </ul>
         </div>
     )
