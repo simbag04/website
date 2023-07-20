590e5d0317684d0fed5ec3ad583b1a87083f1ccf
simbag04 <simbag04@gmail.com>
2023-07-19 17:15:50 -0700

    Fix minor styling bug

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = (props) => {
 
     return (
         <div className="about section" ref={ref}
-            style={{minHeight: `calc(100vh - ${props.headerHeight}px)`}}>
+            style={{minHeight: `calc(100vh - ${props.headerHeight}px + 5px)`}}>
             <div className="title">About</div>
             <div className="about-info">My name is Simran Bagaria. I am a rising sophomore at the University of Washington in Seattle, majoring in Computer Science. I love anything related to technology and education. In addition to being a full time student, I am also a tutor for high school math, from Algebra up till Calculus.</div>
         </div>
