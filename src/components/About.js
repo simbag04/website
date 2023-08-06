0b03ce59bfb0717e611651eef3bf766c9627ada1
simbag04 <simbag04@gmail.com>
2023-08-05 18:35:09 -0700

    Update about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import '../styles/About.css'
 import { useRef, useEffect } from 'react'
+import Simran from '../images/simran-pic.jpg'
 
 const About = (props) => {
     const ref = useRef();
@@ -15,7 +16,8 @@ const About = (props) => {
         <div className="about section" ref={ref}
             style={{minHeight: `calc(100vh - ${props.headerHeight}px + 5px)`}}>
             <div className="title">About</div>
-            <div className="about-info">My name is Simran Bagaria. I am a rising sophomore at the University of Washington in Seattle, majoring in Computer Science. I love anything related to technology and education. In addition to being a full time student, I am also a tutor for high school math, from Algebra up till Calculus.</div>
+            <div className="about-info">Hi there, my name is Simran Bagaria! I am a rising junior at the University of Washington in Seattle, majoring in Computer Science. I am very passionate about technology and its ability to have a positive impact on our world. As an avid self-learner, I am always eager to dive deeper into the Computer Science field, as well as apply my knowledge to solve real-world problems!</div>
+            <img src={Simran} alt=""></img>
         </div>
     )
 }
